15939a08fe4f4f2ddb5ffb1469648c6003707f78
Валентин <astrotrain55@gmail.com>
2024-01-11 20:10:48 +0500

    axios -> node-fetch

--- src/api/fetch.mjs
+++ src/api/fetch.mjs
@@ -1,26 +1,22 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 import { encoding } from '../utils/encodingText.mjs';
 
 export default {
   getQuote() {
-    return axios({
-      method: 'get',
-      url: 'https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=ru',
-    }).then((response) => response.data)
+    return fetch('https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=ru')
+      .then((response) => response.json())
       .then(({ quoteText, quoteAuthor }) => quoteAuthor ? `${quoteText}\n<b>${quoteAuthor}</b>` : quoteText);
   },
   getAdvice() {
-    return axios({
-      method: 'get',
-      url: 'https://fucking-great-advice.ru/api/random',
-    }).then((response) => response.data.text);
+    return fetch('https://fucking-great-advice.ru/api/random')
+      .then((response) => response.json())
+      .then((data) => data.text);
   },
   getRand(typeId) {
-    return axios({
-      method: 'get',
-      url: `http://rzhunemogu.ru/RandJSON.aspx?CType=${typeId}`,
-      responseType: 'arraybuffer',
-    }).then(({ data }) => encoding(data))
+    return fetch(`http://rzhunemogu.ru/RandJSON.aspx?CType=${typeId}`)
+      .then((response) => response.arrayBuffer())
+      .then((data) => Buffer.from(data))
+      .then((data) => encoding(data))
       .then((text) => text.replace('{"content":"', '').replace('"}', ''));
   },
 };
