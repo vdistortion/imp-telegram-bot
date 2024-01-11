72b071354d5322485470ecd44f905608af69ac57
Валентин <astrotrain55@gmail.com>
2024-01-11 21:57:36 +0500

    config bot

--- src/api/bot.mjs
+++ src/api/bot.mjs
@@ -1,4 +1,12 @@
 import TelegramBot from 'telebot';
 import 'dotenv/config';
 
-export default new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+export default new TelegramBot({
+  token: process.env.TELEGRAM_BOT_TOKEN,
+  allowedUpdates: [],
+  polling: {
+    interval: 1000,
+    timeout: 0,
+    retryTimeout: 5000,
+  },
+});
