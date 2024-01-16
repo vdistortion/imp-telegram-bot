c55a2ed4826c51eb26826192dd40c7dde074fdfc
Валентин <astrotrain55@gmail.com>
2024-01-16 21:03:58 +0500

    plugins

--- src/api/bot.js
+++ src/api/bot.js
@@ -1,14 +1,8 @@
 import TelegramBot from 'telebot';
-import 'telebot/plugins/shortReply.js';
-import 'telebot/plugins/regExpMessage.js';
 import 'dotenv/config';
 
 export default new TelegramBot({
   token: process.env.TELEGRAM_BOT_TOKEN,
-  allowedUpdates: [],
-  polling: {
-    interval: 5000,
-    timeout: 5000,
-    retryTimeout: 5000,
-  },
+  pluginFolder: '../plugins/',
+  usePlugins: ['shortReply', 'regExpMessage'],
 });
