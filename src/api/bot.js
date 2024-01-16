f23c65eb1be9848618f78b2d59b78da213b3b8ad
Валентин <astrotrain55@gmail.com>
2024-01-16 21:08:40 +0500

    plugins link

--- src/api/bot.js
+++ src/api/bot.js
@@ -1,8 +1,6 @@
 import TelegramBot from 'telebot';
+import '../../node_modules/telebot/plugins/regExpMessage.js';
+import '../../node_modules/telebot/plugins/shortReply.js';
 import 'dotenv/config';
 
-export default new TelegramBot({
-  token: process.env.TELEGRAM_BOT_TOKEN,
-  pluginFolder: '../plugins/',
-  usePlugins: ['shortReply', 'regExpMessage'],
-});
+export default new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
